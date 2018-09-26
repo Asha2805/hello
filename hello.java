fff2972deaaf6bc58ccbcf0d2ca97ed31c4e5c8f
Asha2805 <43291441+Asha2805@users.noreply.github.com>
2018-09-26 08:07:30 -0400

    Update and rename README.md to hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+# import java.util.*;
+public class hello
+{
+public static void main(String[] args)
+{
+
+int n;
+Scanner w=new Scanner(System.in);
+
+n=w.nextInt();
+for(int i=1;i<=n;i++)
+{
+system.out.println("Hello");
+}
+}
+}
